20f9f26cd7d9d83bb5d9391c75da4a721d57a953
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-05-18 23:20:59 +0200

    disable llvm in 32-bit config

--- conf32.sh
+++ conf32.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-meson .. --prefix ~/mesa-bin --libdir ~/mesa-bin/lib/i386-linux-gnu/ --cross-file=../cross_file.txt -Dgallium-drivers=iris -Dvulkan-drivers=intel -Ddri-drivers=i965 "$@"
+meson .. --prefix ~/mesa-bin --libdir ~/mesa-bin/lib/i386-linux-gnu/ --cross-file=../cross_file.txt -Dgallium-drivers=iris -Dvulkan-drivers=intel -Ddri-drivers=i965 -Dllvm=false "$@"
