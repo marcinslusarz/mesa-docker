aeacc93e42024cdc80140aaa4070842d97edf047
Marcin Ślusarz <marcin.slusarz@intel.com>
2021-07-12 09:49:27 +0200

    LIBGL_DRIVERS_PATH is not needed anymore

--- mygl_rh.sh
+++ mygl_rh.sh
@@ -3,10 +3,6 @@
 export LD_LIBRARY_PATH=~/mesa-bin/lib64/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=~/mesa-bin/lib/:$LD_LIBRARY_PATH
 
-export LIBGL_DRIVERS_PATH=
-export LIBGL_DRIVERS_PATH=~/mesa-bin/lib64/dri/:$LIBGL_DRIVERS_PATH
-export LIBGL_DRIVERS_PATH=~/mesa-bin/lib/dri/:$LIBGL_DRIVERS_PATH
-
 export VK_ICD_FILENAMES=~/mesa-bin/share/vulkan/icd.d/intel_icd.i686.json:~/mesa-bin/share/vulkan/icd.d/intel_icd.x86_64.json
 
 "$@"
